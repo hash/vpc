17db53a7b0189881479d010da088fcee482dfa15
K4zaam <kazaam@o2.pl>
2011-05-15 22:17:15 +0200

    szkielety projektow fixes #4 #5

---
+++ src/vpc_conf/src/vpc_conf/Vpc_confApp.java
@@ -0,0 +1,44 @@
+/*
+ * Vpc_confApp.java
+ */
+
+package vpc_conf;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class Vpc_confApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new Vpc_confView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of Vpc_confApp
+     */
+    public static Vpc_confApp getApplication() {
+        return Application.getInstance(Vpc_confApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(Vpc_confApp.class, args);
+    }
+}
---
+++ src/vpc_service/src/vpc_service/Vpc_service.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vpc_service;
+
+/**
+ *
+ * @author kazaam
+ */
+public class Vpc_service {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
