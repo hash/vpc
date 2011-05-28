6ddc85d7e245b5e045157dbf6f255ba7df4efdbf
Kazaam <kazaam@o2.pl>
2011-05-28 19:27:54 +0200

    aktualka. powinno dzialac juz ok, ale liby musicie sami dolozyc. to tak, w glownym katalogu projektu robimy folder lib/ i tam wrzucamy
    js.jar
    jsapi.jar
    sphinx4.jar
    WSJ_8gau_13dCep_16k_40mel_130Hz_6800Hz.jar
    
    wszystko znajdziecie w folderze lib/ paczki ze sphixem, ktora sciagneliscie. jak netbeans nie wyczai tego po otwarciu projektu to w nb w drzewie projektu prawym na Libraries -> Add JAR/Folder i dodajemy wszystkie w/w jary. potem Shift+F11 i mozna sie bawic.

--- src/vpc_service/src/vpc_service/Vpc_service.java
+++ src/vpc_service/src/vpc_service/Vpc_service.java
@@ -1,19 +1,64 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package vpc_service;
 
-/**
- *
- * @author kazaam
- */
+import edu.cmu.sphinx.frontend.util.Microphone;
+import edu.cmu.sphinx.recognizer.Recognizer;
+import edu.cmu.sphinx.result.Result;
+import edu.cmu.sphinx.util.props.ConfigurationManager;
+
+
 public class Vpc_service {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args){
-        // TODO code application logic here
+    public static void main(String[] args) {        
+        
+        ConfigurationManager cm;
+
+        if (args.length > 0) {
+            cm = new ConfigurationManager(args[0]);
+        } else {
+            cm = new ConfigurationManager(Vpc_service.class.getResource("vpc_service.config.xml"));
+        }
+
+        Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
+        recognizer.allocate();
+
+        // start the microphone or exit if the programm if this is not possible
+        Microphone microphone = (Microphone) cm.lookup("microphone");
+        if (!microphone.startRecording()) {
+            System.out.println("Cannot start microphone.");
+            recognizer.deallocate();
+            System.exit(1);
+        }
+
+        System.out.println("Say: (chrome)");
+
+        // loop the recognition until the programm exits.
+        while (true) {
+            System.out.println("Start speaking. Press Ctrl-C to quit.\n");
+
+            Result result = recognizer.recognize();
+
+            if (result != null) {
+                String resultText = result.getBestFinalResultNoFiller();
+                System.out.println("You said: " + resultText + '\n');
+                
+                //String url = "chrome";
+                String os = System.getProperty("os.name").toLowerCase();
+                Runtime rt = Runtime.getRuntime();
+
+                try
+                {
+                    // this doesn't support showing urls in the form of "page.html#nameLink" 
+                    rt.exec( "rundll32 url.dll,FileProtocolHandler " + resultText);
+                }
+                catch (Exception e)
+                {
+                    return;
+                }  
+                
+            } else {
+                System.out.println("I can't hear what you said.\n");
+            }
+        }
     }
 }
