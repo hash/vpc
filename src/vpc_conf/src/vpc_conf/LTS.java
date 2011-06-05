01ff6eb7e839a05b60ef16eb3a5c0d3e862c4081
Kazaam <kazaam@o2.pl>
2011-06-05 22:28:54 +0200

    Ostatnie komentarze do kodu i porzadki.

--- src/vpc_conf/src/vpc_conf/LTS.java
+++ src/vpc_conf/src/vpc_conf/LTS.java
@@ -6,52 +6,60 @@ import java.net.URL;
 import com.sun.speech.freetts.lexicon.LetterToSound;
 import com.sun.speech.freetts.lexicon.LetterToSoundImpl;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
-
+/**
+ * Klasa do kontroli słownika, konwersja słów na postać słownikową, sprawdzanie
+ * dostępności słów. Wykorzystuje narzędzia do wydobywania fonemów z biblioteki FreeTTS.
+ */
 public class LTS {
 
-    LetterToSound lts = null;
-    ArrayList<String> words, cmdLTS, cmdWords;
-    boolean exists = true;
+    //Obiekt konwertera
+    private LetterToSound lts = null;
+    //Listy słów w słowniku oraz słów komendy do dodania.
+    private ArrayList<String> words, cmdLTS;
+    //Zmienna pomocnicza
+    private boolean exists = true;
 
+    /**
+     * Konstruktor. Inizjalizuje obiekty pól do obsługi konwertera.
+     */
     public LTS() {    
         words = new ArrayList<String>();
         cmdLTS = new ArrayList<String>();
-        cmdWords = new ArrayList<String>();
     }
-    //konwersja słów z komendy do postaci slownikowej
+    /**
+     * Rozdziela podaną komendę na wyrazy i konwertuje na postać słownikową
+     * @param word podana komenda do przekonwertowania
+     * @throws IOException błąd odczytu konwertera cmulex
+     */
     public void getLTS(String word) throws IOException{
+        //inicjalizacja konwertera cmulex do wydobywania fonemow ze słowa
         lts = new LetterToSoundImpl(new URL("file:cmulex/cmulex_lts.bin"),
                                     true);
+        //jeśli komenda składa się z więcej niżjednego słowa rozdzielamy je i
+        //sprawdzamy każde słowo osobno.
         if (word.contains(" ")) {
-            System.out.println("komenda złożona");
             for (int w = 0 ; w < word.split(" ").length ; w++) {
                 splitWord(word.split(" ")[w]);
-                cmdWords.add(word.split(" ")[w]);
             }
         } else {
-            System.out.println("komenda pojedyncza");
-            splitWord(word);
-            cmdWords.add(word);                
-        }
-        System.out.println(" ");
-        for(int i = 0; i < cmdLTS.size(); i++)
-            System.out.println(cmdLTS.get(i));
-        for(int i = 0; i < cmdWords.size(); i++)
-            System.out.println(cmdWords.get(i));
-            
+            //w przypadku jednego słowa od razu przechodzimy do konwersji.
+            splitWord(word);              
+        }         
     }
-    //konwersja pojedynczego słowa
+    //konwersja pojedynczego słowa na postać słownikową
+    //np: befsztyk -> BEFSZTYK B EH F SH T IH K
     private void splitWord(String word){
         String[] ltspM_phone_array;
         String phones = "";
         ltspM_phone_array = lts.getPhones(word, null);
-        System.out.print(
-                "Word: " + word.toUpperCase() + " Phones: ");
-        phones += word.toUpperCase();
+        //System.out.print(
+        //        "Word: " + word.toUpperCase() + " Phones: ");
+        phones += word.toUpperCase(); //słowa muszą być wpisane do słownika dużymi literami
         phones += " ";
+        //zmiana fonemów AX na AH - konwerter cmulex działa w nieco innym standardzie niż
+        // Sphinx i przez niektóre fonemy się różnią - różnica między brzmieniem
+        // AX i AH jest w tym wypadku pomijalna
         for (int i = 0 ; i < ltspM_phone_array.length ; i++) {
             if (ltspM_phone_array[i].equals("ax")) {
                 ltspM_phone_array[i] = "ah";
@@ -62,27 +70,26 @@ public class LTS {
                                                                       ltspM_phone_array[i].
                         length() - 1);
             }
-            System.out.print(ltspM_phone_array[i].toUpperCase() + " ");
+            //System.out.print(ltspM_phone_array[i].toUpperCase() + " ");
             phones += " ";
             phones += ltspM_phone_array[i].toUpperCase();
         }
-        cmdLTS.add(phones);
-        System.out.println();
-        System.out.println("sowo dodane do listy");        
+        cmdLTS.add(phones);    
     }
-    
-    public boolean addToDict() throws FileNotFoundException, IOException
+    /**
+     * Dodawanie otrzymanych słów do słownika pod odpowiednią postacią
+     * @throws FileNotFoundException brak pliku słownika
+     * @throws IOException błąd odczytu/zapisu pliku
+     */
+    public void addToDict() throws FileNotFoundException, IOException
     {
         for(int i = 0; i < cmdLTS.size(); i++)
-            words.add(cmdLTS.get(i));
-        System.out.println("" + words.size() + " słów");     
-        saveDict();
-        System.out.println("słownik zpaisany");     
+            words.add(cmdLTS.get(i));  
+        saveDict();   
         exists = true;
         cmdLTS.clear();
-        cmdWords.clear();
-        return true;
     }
+    //wczytywanie pliku słownika do pamięci - lista words
     private void readDict() throws FileNotFoundException, IOException
     {
         words.clear();
@@ -92,9 +99,7 @@ public class LTS {
         while((s = br.readLine()) != null) {
             for(int i = 0; i < cmdLTS.size(); i++) {
                     if(s.split(" ")[0].equals(cmdLTS.get(i).split(" ")[0])) {
-                        cmdLTS.remove(i);        
-                        System.out.println(s + "wykryto słowo w "
-                                + "słowniku, usuwanie z listy do dodania: " + cmdWords.get(i));
+                        cmdLTS.remove(i);   
                     }
             }
             words.add(s);
@@ -103,9 +108,8 @@ public class LTS {
         else exists = false;
         br.close();
         fr.close();      
-        System.out.println("\nwczytalem plik slownika...");
-        System.out.println("" + words.size() + " słów"); 
     }
+    //zapisywanie listy words do pliku słownika
     private void saveDict() throws FileNotFoundException, IOException
     {
         FileWriter fw = new FileWriter("../config/WSJ_8gau_13dCep_16k_40mel_130Hz_6800Hz/dict/cmudict.0.6d");
@@ -117,8 +121,14 @@ public class LTS {
         }
         bw.close();
         fw.close();  
-        System.out.println("zapisalem do slownika");
     }
+    /**
+     * Sprawdza czy podane w komendzie słowa znajdują się w słowniku. Selekcja słów
+     * przeprowadzana jest podczas wczytywania pliku słownika do pamięci
+     * @param cmd podana komenda
+     * @return <code>true</code> - jeśli wszystkie ze słów znajdują sie w słowniku
+     *         <code>false</code> - jeśli przynajmniej jednego ze słów nie ma w słowniku
+     */
     public boolean cmdExists(String cmd)
     {
         try {
